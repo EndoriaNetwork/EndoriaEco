942c35d5702561fa1fb62340da28a56e6a39212e
TinouHD <ftb.tinouhd.yt@gmail.com>
2018-10-19 20:02:39 +0200

    DB connect commit

--- src/fr/tinouhd/endoriaeco/Main.java
+++ src/fr/tinouhd/endoriaeco/Main.java
@@ -1,12 +1,15 @@
 package fr.tinouhd.endoriaeco;
 
+import fr.hugob147.endorialobby.mysql.MySQL;
 import fr.tinouhd.endoriaeco.commands.EcoCmd;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
 {
+	fr.hugob147.endorialobby.mysql.MySQL mysql = new MySQL();
 	@Override public void onEnable()
 	{
+		this.mysql.connect("localhost", "endoria", 3306, "Endoria", "hugo34");
 		getCommand("eco").setExecutor(new EcoCmd());
 	}
 
